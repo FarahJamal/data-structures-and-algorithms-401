b074a9ff0891a77b96dd28d27a852e0fb4a0f8c6
FarahJamal <farahsarese@gmail.com>
2021-11-01 18:06:45 +0200

    finish with walkthrough

---
+++ MergeSort/README.md
@@ -0,0 +1,6 @@
+# merge sort
+![](https://i.ibb.co/3YHXwMN/Whiteboard-13.png)
+## create function called merge Sort that sort array using mergeSort alogorthim
+
+## step by step walk through 
+![](pp.gif)
---
+++ MergeSort/lib/src/main/java/MergeSort/MS.java
@@ -0,0 +1,51 @@
+package MergeSort;
+
+public class MS {
+    public static void mergeSort(int[] a) {
+        int n=a.length;
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l);
+        mergeSort(r);
+
+        merge(a, l, r);
+    }
+    public static void merge(int[] a, int[] l, int[] r) {
+int left=l.length;
+        int right=r.length;
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
